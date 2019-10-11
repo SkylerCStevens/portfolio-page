a17cbb0c39b50528d249ffabf38c0da3a08805c1
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-11 14:45:05 -0400

    Add name and address

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,17 +3,30 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="contact" id="contact">
-            <h2 className="page-header">Contact Me</h2>
-            <form>
-                <label>Name:</label>
-                <input placeholder="Name"></input>
+            <div className="container">
+            <h2 className="page-header">CONTACT ME</h2>
+            <div className="contact-content">
+            <div className="name-address">
+                <span className="contact-name">SKYLER STEVENS</span>
+                <address>Charlotte, North Carolina</address>
+                <p className="contact-p">Have any questions? Want to work together? Let's talk!</p>
+                <a href="https://github.com/SkylerCStevens" target="_blank"><img className="icon github" src="https://cdn4.iconfinder.com/data/icons/liu-square-blac/60/github-square-social-media-512.png" alt="GitHub icon"></img></a>
+                <a href="https://www.linkedin.com/in/skyler-stevens-789115195/" target="_blank"><img className="icon" src="https://cdn0.iconfinder.com/data/icons/global-top-brands/430/linkedin-logo-1-512.png" alt="LinkedIn icon"></img></a>
+            </div>
+            <form className="contact-form column-right">
+                <label className="form-label" for="user_name">NAME</label>
+                <input className="form-control" id="user_name" placeholder="Name" required></input>
                 
-                <label>Email</label>
-                <input type="email"></input>
+                <label className="form-label" for="user_email">EMAIL</label>
+                <input className="form-control" id="user_email" type="email" placeholder="E-mail" required></input>
 
-                <label>Message</label>
-                <input type="text"></input>
+                <label className="form-label" for="user_message">MESSAGE</label>
+                <textarea className="form-control" id="user_message" type="text" placeholder="Message" required></textarea>
+
+                <button className="btn btn-light" type="submit" value="submit">SEND</button>
             </form>
+            </div>
+            </div>
         </div>
     )
 };
