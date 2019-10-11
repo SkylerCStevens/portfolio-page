4d6d524f52f3e96968ad67aeb98dc83f0f51468a
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-11 14:44:18 -0400

    Add photo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import headshot from '../images/headshot.png'
 
 const About = () => {
     return (
         <div className="container about" id="about">
-            <h2 className="page-header">About Me</h2>
+            <h2 className="page-header">ABOUT ME</h2>
             <div className="column-left">
-            <img src="https://i.pinimg.com/originals/b1/1a/cb/b11acb20797de687a98396435c9ec0ee.jpg" alt="Photo of the author of website smiling with floating lights behind them"></img>
+            <img src={headshot} alt="Photo of the author of website smiling with floating lights behind them"></img>
             </div>
             <div className="column-right">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas integer eget aliquet nibh praesent tristique magna sit. Pellentesque elit eget gravida cum sociis natoque penatibus et. Libero id faucibus nisl tincidunt eget nullam. Risus feugiat in ante metus dictum. Felis eget nunc lobortis mattis aliquam faucibus. Urna duis convallis convallis tellus. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae. Fringilla est ullamcorper eget nulla. Pretium lectus quam id leo in vitae turpis massa sed. Mattis pellentesque id nibh tortor.</p>
