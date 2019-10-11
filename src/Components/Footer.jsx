f1a2d2204169546f2b4fcddf7811956133fa3422
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-11 14:45:31 -0400

    Add update and copyright

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <span>Footer</span>
-        </div>
+        <footer role="contentinfo">
+            <span className="copyright">Copyright 2019</span>
+            <span className="update">Last Updated October 2019</span>
+        </footer>
     )
 };
 
