d176001320fb3fbcd1efdc63d0a8e1ba79404288
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-11 17:12:00 -0400

    Add ecommerce page to projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import guessingGame from '../images/guessing-game.png';
+import ecommerce from '../images/ecommerce.png';
 
 const Portfolio = () => {
     return (
@@ -15,7 +16,7 @@ const Portfolio = () => {
             </ul>
             <div className="row image-row">
             <div className="image-container text-center">
-                <img className="project-img" src={guessingGame} alt="This is a project"></img>
+                <img className="project-img" src={guessingGame} alt="Screenshot of number guessing game"></img>
                     <div className="middle">
                         <p className="text">Number Guessing Game</p>
                         <p className="text">ReactJS</p>
@@ -26,13 +27,12 @@ const Portfolio = () => {
                     </div>
                     </div>
                     <div className="image-container text-center">
-                <img className="project-img" src="https://www.cabarethotspot.com/wp-content/uploads/2018/08/ComingSoon-1280x640.jpg" alt="This is a project"></img>
+                <img className="project-img" src={ecommerce} alt="Screenshot of ecommerce site"></img>
                     <div className="middle">
-                        <p className="text"></p>
-                        <p className="text"></p>
+                        <p className="text">Ecommerce Mock Site</p>
+                        <p className="text">React, NodeJS, MySQL</p>
                         <div className="project-link-container">
-                        <a className="project-link" href="">GitHub</a>
-                        <a className="project-link" href="">Netlify</a>
+                        <a className="project-link" href="https://github.com/SkylerCStevens/ecommerce-site" target="_blank">GitHub</a>
                         </div>
                     </div>
                     </div>
