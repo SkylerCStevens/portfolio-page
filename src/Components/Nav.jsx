59257ec81396d5328d2f0114edb3a3ad93614318
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-11 14:47:08 -0400

    Capitalize name and buttons

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div className="nav">
-            <h1 className="nav-name">Skyler Stevens</h1>
+        <nav role="navigation" className="nav">
+            <h1 className="nav-name">SKYLER STEVENS</h1>
             <ul className="nav-list ml-auto mt-auto">
-                <li className="btn home-nav nav-btn"><a href="#home">Home</a></li>
-                <li className="btn about-nav nav-btn"><a href="#about">About</a></li>
-                <li className="btn portfolio-nav nav-btn"><a href="#portfolio">Portfolio</a></li>
-                <li className="btn contact-nav nav-btn"><a href="#contact">Contact</a></li>
+                <li className="btn home-nav nav-btn"><a href="#home">HOME</a></li>
+                <li className="btn about-nav nav-btn"><a href="#about">ABOUT</a></li>
+                <li className="btn portfolio-nav nav-btn"><a href="#portfolio">PORTFOLIO</a></li>
+                <li className="btn contact-nav nav-btn"><a href="#contact">CONTACT</a></li>
             </ul>
-        </div>
+        </nav>
     )
 };
 
