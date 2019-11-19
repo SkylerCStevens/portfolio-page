0e042e40b4e1f51fcee369bbc3991b1ab85ec61b
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-11-18 23:25:23 -0500

    Add projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -7,20 +7,17 @@ class Portfolio extends Component {
         projects: Projects
     }
 
+    handleButton = (e) => {
+        
+    }
+
     render () {
        const { projects } = this.state
        console.log(projects)
     return (
-        <div className="portfolio" id="portfolio">
+        <div className="portfolio mb-5" id="portfolio">
             <div className="container">
-            <h2 className="page-header">PROJECTS</h2>
-            <ul className="tech-list text-center">
-                <li><button className="btn btn-link tech-filter">HTML/CSS</button></li>
-                <li><button className="btn btn-link tech-filter">JavaScript</button></li>
-                <li><button className="btn btn-link tech-filter">ReactJS</button></li>
-                <li><button className="btn btn-link tech-filter">MySQL</button></li>
-                <li><button className="btn btn-link tech-filter">NodeJS</button></li>
-            </ul>
+            <h2 className="page-header">PROJECTS</h2> 
             <div className="row">
             {projects.map(project => (
                 <div className="image-row">
@@ -28,9 +25,9 @@ class Portfolio extends Component {
                     <img className="project-img" src={project.image} alt={project.name}></img>
                         <div className="middle">
                             <p className="text inline">{project.name}, </p>
-                            {project.languages.forEach(language => (
-                                <p className="text">{language}</p>
-                            ))}
+                            <p className="text">{project.languages.map(language => (
+                                <div>{language}</div>
+                            ))}</p>
                             <div className="project-link-container">
                             <a className="project-link" href={project.github} target="_blank" rel="noopener noreferrer">GitHub</a>
                             {project.netlify ? <a className="project-link" href={project.netlify} target="_blank" rel="noopener noreferrer">Netlify</a> : "" }
